cf4ff0b70c833e368def9fb7f2bad11266ec05fa
ORatinskiy <234854@gmail.com>
2020-10-21 09:04:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = '';
+    result = n.toString();
+    result = result.split("");
+    result = result.reverse();
+    result = result.join("");
+    result = parseInt(result);
+    return result;
 }
